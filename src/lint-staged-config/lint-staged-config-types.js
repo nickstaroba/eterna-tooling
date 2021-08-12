67b2c9eaf24709dfa81ba5dee8ba3d5c4748ac39
Nick Staroba <nick@nickstaroba.com>
2021-08-12 10:56:15 -0400

    chore(lint-staged): refactoring to use updated build utilities

--- src/lint-staged-config/lint-staged-config-types.js
+++
@@ -1,5 +0,0 @@
-export const LintStagedConfigTypes = Object.freeze({
-  ESLINT: "lint-staged-config-eslint",
-  PRETTIER: "lint-staged-config-prettier",
-  STYLELINT: "lint-staged-config-stylelint",
-});
