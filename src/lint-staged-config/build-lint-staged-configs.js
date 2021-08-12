29336f5c048fb45df369c86e9366617b6185459d
Nick Staroba <nick@nickstaroba.com>
2021-08-12 10:56:15 -0400

    chore(lint-staged): refactoring to use updated build utilities

--- src/lint-staged-config/build-lint-staged-configs.js
+++ src/lint-staged-config/build-lint-staged-configs.js
@@ -1,37 +1,22 @@
-import path from "path";
+import { buildConfig } from "../build-config";
+import { updateDependencies } from "../update-dependencies";
 
-import { buildPackage } from "../build-package";
-import { configurePackageJson } from "../configure-package-json";
-import { LintStagedConfigTypes } from "./lint-staged-config-types";
-import eslintPackageJson from "./package.lint-staged-config-eslint.json";
-import prettierPackageJson from "./package.lint-staged-config-prettier.json";
-import stylelintPackageJson from "./package.lint-staged-config-stylelint.json";
-
-const configPaths = Object.fromEntries(
-  Object.entries(LintStagedConfigTypes).map(([key, value]) => [
-    key,
-    path.resolve(__dirname, `${value}.js`),
-  ])
-);
+export const LintStagedConfigTypes = Object.freeze({
+  ESLINT: "eslint",
+  PRETTIER: "prettier",
+  STYLELINT: "stylelint",
+});
 
 export function buildLintStagedConfigs(done) {
-  configurePackageJson(LintStagedConfigTypes.ESLINT, eslintPackageJson).then(
-    () => buildPackage(configPaths.ESLINT, LintStagedConfigTypes.ESLINT)
-  );
+  Object.values(LintStagedConfigTypes).forEach((configType) => {
+    buildConfig("lint-staged", configType);
+  });
+  done();
+}
 
-  configurePackageJson(
-    LintStagedConfigTypes.PRETTIER,
-    prettierPackageJson
-  ).then(() =>
-    buildPackage(configPaths.PRETTIER, LintStagedConfigTypes.PRETTIER)
-  );
-
-  configurePackageJson(
-    LintStagedConfigTypes.STYLELINT,
-    stylelintPackageJson
-  ).then(() =>
-    buildPackage(configPaths.STYLELINT, LintStagedConfigTypes.STYLELINT)
-  );
-
+export function updateLintStagedDependencies(done) {
+  Object.values(LintStagedConfigTypes).forEach((configType) => {
+    updateDependencies("lint-staged", configType);
+  });
   done();
 }
