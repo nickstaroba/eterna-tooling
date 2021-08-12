a10bb67014ffb78a195d9ed9f5117ed40040ba33
Nick Staroba <nick@nickstaroba.com>
2021-08-12 10:57:08 -0400

    fix(typescript): allowing javascript

--- src/typescript-config/build-typescript-config.js
+++ src/typescript-config/build-typescript-config.js
@@ -1,19 +1,23 @@
 import { dest, src } from "gulp";
 import path from "path";
 
-import { configurePackageJson, outputDir } from "../configure-package-json";
+import { configurePackageJson } from "../configure-package-json";
+import { updateDependencies } from "../update-dependencies";
 import packageJson from "./package.typescript-config.json";
 
 const configPath = path.resolve(__dirname, "tsconfig.json");
 const packageName = "typescript-config";
 
 function buildConfig() {
-  return src(configPath).pipe(dest(`${outputDir}/${packageName}`));
+  return src(configPath).pipe(dest(`./packages/${packageName}`));
 }
 
 export function buildTypescriptConfig(done) {
-  configurePackageJson(packageName, packageJson).then(() =>
-    buildConfig(configPath, packageName)
-  );
+  configurePackageJson(packageName, packageJson).then(() => buildConfig());
+  done();
+}
+
+export function updateTypescriptDependencies(done) {
+  updateDependencies("typescript");
   done();
 }
